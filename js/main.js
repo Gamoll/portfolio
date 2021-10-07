48cdc22bcfad1a9440625758a855c7b787fe80cc
Gamoll <gaia.moller@hotmail.com>
2021-10-07 11:57:44 +0200

    firstdraft

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var i = 0; 			
+var images = [];	
+var time = 3000;	
+	 
+// Image List
+images[0] = "/img/process1.jpg";
+images[1] = "/img/process2.jpg";
+images[2] = "/img/process3.jpg";
+images[3] = "/img/process4.jpg";
+
+// Change Image
+function changeImg(){
+	document.slide.src = images[i];
+
+	
+	if(i < images.length - 1){
+	
+	  i++; 
+	} else { 
+		
+		i = 0;
+	}
+
+	
+	setTimeout("changeImg()", time);
+}
+
+// Run function when page loads
+window.onload=changeImg;
