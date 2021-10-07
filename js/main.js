e5fb446e1289140dc4cc61f32c5d536a2b1af9e4
Gamoll <gaia.moller@hotmail.com>
2021-10-07 12:56:15 +0200

    newurl

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ var images = [];
 var time = 3000;	
 	 
 // Image List
-images[0] = "../img/hero1024x768.jpg";
-images[1] = "../img/process2.jpg";
-images[2] = "../img/process3.jpg";
-images[3] = "../img/process4.jpg";
+images[0] = "https://images.pexels.com/photos/7096/people-woman-coffee-meeting.jpg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260";
+images[1] = "https://images.pexels.com/photos/7095/people-coffee-notes-tea.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+images[2] = "https://images.pexels.com/photos/3153198/pexels-photo-3153198.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+images[3] = "https://images.pexels.com/photos/4348404/pexels-photo-4348404.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
 
 // Change Image
 function changeImg(){
